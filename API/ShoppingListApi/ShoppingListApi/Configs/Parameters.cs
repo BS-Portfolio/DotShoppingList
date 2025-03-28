ee52801ea9b47ab5716f81fc589820989bbb1da6
Leon Marzoll <lmarzoll@axro.de>
2025-03-28 11:27:10 +0100

    #Basic Structure

---
+++ API/ShoppingListApi/ShoppingListApi/Configs/Parameters.cs
@@ -0,0 +1,8 @@
+﻿namespace ShoppingListApi.Configs;
+
+public static class Parameters {
+    
+    
+    // Test Parameter to show how to declare a parameter
+    public static string ShoppingListApi = "ShoppingListApi";
+}
