8b4726844150969899a67a8631a868a311a7b7f3
Leon Marzoll <lmarzoll@axro.de>
2025-03-28 11:48:19 +0100

    Created basic structure and installed nuget paket xunit

---
+++ ShoppingList/ShoppingList.Server/Tests/UnitTests.cs
@@ -0,0 +1,23 @@
+﻿using Xunit;
+
+namespace ShoppingList.Server.Tests;
+
+public class UnitTests
+{
+    // Example Unit Test to show how to write a test
+    
+    // [Fact]
+    // public void Divide_ShouldReturnCorrectQuotient()
+    // {
+    //     // Arrange
+    //     int a = 10;
+    //     int b = 2;
+    //     double expected = 5.0;
+    //
+    //     // Act
+    //     double result = _calculator.Divide(a, b);
+    //
+    //     // Assert
+    //     Assert.Equal(expected, result);
+    // }
+}
