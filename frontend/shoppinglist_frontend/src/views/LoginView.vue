37e5a3ed060d07e954458d5b79295651da1edf10
Fynn Danger <fdanger@axro.com>
2025-04-03 12:08:34 +0200

    refactor, fixed list scale

--- frontend/shoppinglist_frontend/src/views/LoginView.vue
+++ frontend/shoppinglist_frontend/src/views/LoginView.vue
@@ -1,21 +1,25 @@
 <script setup lang="ts">
-import { ref } from 'vue'
-import { useAuthStore } from '@/stores/auth'
-import { useRouter } from 'vue-router'
+import { ref } from 'vue';
+import { useAuthStore } from '@/stores/auth';
+import { useRouter } from 'vue-router';
 
-const authStore = useAuthStore()
-const router = useRouter()
+const authStore = useAuthStore();
+const router = useRouter();
 
-const username = ref('')
-const password = ref('')
+const username = ref('');
+const password = ref('');
+
+const saveToSession = (key: string, value: any) => localStorage.setItem(key, JSON.stringify(value));
+
+const handleLoginSuccess = () => {
+  saveToSession('isAuthenticated', true);
+  router.push('/');
+};
 
 const login = () => {
-  authStore.login(username.value, password.value)
-  if (authStore.isAuthenticated) {
-    localStorage.setItem('isAuthenticated', 'true')
-    router.push('/')
-  }
-}
+  authStore.login(username.value, password.value);
+  if (authStore.isAuthenticated) handleLoginSuccess();
+};
 </script>
 
 <template>
