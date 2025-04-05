b144324ea078c6a1a7397c9ad21d8cfb02dd1585
milad-dg <milad.d.ghane@live.com>
2025-04-05 11:52:56 +0200

    http get completed

--- ShoppingListApi/Controllers/ShoppingListController.cs
+++ ShoppingListApi/Controllers/ShoppingListController.cs
@@ -1,3 +1,5 @@
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.InteropServices;
 using System.Text;
 using Azure.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -26,7 +28,10 @@ public class ShoppingListController : ControllerBase
     }
 
     [HttpGet]
-    [AdminEndpoint]
+    [PublicEndpoint]
+    [ProducesResponseType<string>(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType<List<UserRole>>(StatusCodes.Status200OK)]
     [Route("UserRole/all")]
     public async Task<ActionResult> GetUserRoles()
     {
@@ -60,80 +65,11 @@ public class ShoppingListController : ControllerBase
         }
     }
 
-    [HttpPost]
-    [AdminEndpoint]
-    [Route("UserRole")]
-    public async Task<ActionResult> AddUserRole([FromBody] UserRolePost userRolePost)
-    {
-        try
-        {
-            var (success, addedUserRoleId) = await _databaseService.SqlConnectionHandler<UserRolePost, (bool, Guid?)>(
-                (input, connection) => _databaseService.AddUserRole(connection, input), userRolePost);
-
-            if (success is false)
-            {
-                return StatusCode(StatusCodes.Status500InternalServerError,
-                    "Du to an internal error, your request could not be processed.");
-            }
-
-            return CreatedAtAction(nameof(AddUserRole), addedUserRoleId);
-        }
-        catch (NumberedException nEx)
-        {
-            _logger.LogWithLevel(LogLevel.Error, nEx, nEx.ErrorNumber, nEx.Message,
-                nameof(ShoppingListController), nameof(AddUserRole));
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                "Du to an internal error, your request could not be processed.");
-        }
-        catch (Exception e)
-        {
-            var numberedException = new NumberedException(e);
-            _logger.LogWithLevel(LogLevel.Error, e, numberedException.ErrorNumber, numberedException.Message,
-                nameof(ShoppingListController), nameof(AddUserRole));
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                "Du to an internal error, your request could not be processed.");
-        }
-    }
-
-    [HttpPost]
-    [PublicEndpoint]
-    [Route("User")]
-    public async Task<ActionResult> AddNewUser([FromBody] ListUserPost userPost)
-    {
-        try
-        {
-            var userPostExtended = new ListUserPostExtended(userPost);
-
-            var (success, addedUserId) =
-                await _databaseService.SqlConnectionHandler<ListUserPostExtended, (bool, Guid?)>(
-                    (input, connection) => _databaseService.AddUser(input, connection), userPostExtended);
-
-            if (success is false || addedUserId is null)
-            {
-                return Problem("Due to an internal error, you request could not be processed.");
-            }
-
-            return Ok(addedUserId);
-        }
-        catch (NumberedException nEx)
-        {
-            _logger.LogWithLevel(LogLevel.Error, nEx, nEx.ErrorNumber, nEx.Message,
-                nameof(ShoppingListController), nameof(AddUserRole));
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                "Du to an internal error, your request could not be processed.");
-        }
-        catch (Exception e)
-        {
-            var numberedException = new NumberedException(e);
-            _logger.LogWithLevel(LogLevel.Error, e, numberedException.ErrorNumber, numberedException.Message,
-                nameof(ShoppingListController), nameof(AddNewUser));
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                "Du to an internal error, your request could not be processed.");
-        }
-    }
-
     [HttpGet]
     [AdminEndpoint]
+    [ProducesResponseType<string>(StatusCodes.Status404NotFound)]
+    [ProducesResponseType<ListUser>(StatusCodes.Status200OK)]
+    [ProducesResponseType<string>(StatusCodes.Status500InternalServerError)]
     [Route("User/EmailAddress/{emailAddress}")]
     public async Task<ActionResult> GetUserByEmail([FromRoute] string emailAddress)
     {
@@ -168,11 +104,228 @@ public class ShoppingListController : ControllerBase
         }
     }
 
+    [HttpGet]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType<List<ShoppingList>>(StatusCodes.Status200OK)]
+    [ProducesResponseType<string>(StatusCodes.Status500InternalServerError)]
+    [Route("User/{userId:Guid}/ShoppingList/all")]
+    public async Task<ActionResult> GetShoppingListsForUser([FromRoute] Guid userId)
+    {
+        try
+        {
+            var shoppingLists = await _databaseService.SqlConnectionHandler<Guid, List<ShoppingList>>(
+                async (id, connection) => await _databaseService.HandleShoppingListsFetchForUser(id, connection),
+                userId
+            );
+
+            if (shoppingLists.Count == 0)
+            {
+                return NoContent();
+            }
+
+            return Ok(shoppingLists);
+        }
+        catch (NumberedException)
+        {
+            throw;
+        }
+        catch (Exception e)
+        {
+            var numberedException = new NumberedException(e);
+            _logger.LogWithLevel(LogLevel.Error, e, numberedException.ErrorNumber, numberedException.Message,
+                nameof(DatabaseService), nameof(GetShoppingListsForUser));
+            throw numberedException;
+        }
+    }
+
+    [HttpGet]
+    [AdminEndpoint]
+    [ProducesResponseType<string>(StatusCodes.Status500InternalServerError)]
+    [ProducesResponseType<ListUser>(StatusCodes.Status200OK)]
+    [ProducesResponseType<string>(StatusCodes.Status404NotFound)]
+    [Route("User/{userId:Guid}")]
+    public async Task<ActionResult> GetUserById(Guid userId)
+    {
+        try
+        {
+            var user = await _databaseService.SqlConnectionHandler<Guid, ListUser?>(
+                (input, connection) => _databaseService.GetUserById(input, connection),
+                userId
+            );
+
+            if (user is null)
+            {
+                return NotFound($"User with the provided ID not found");
+            }
+
+            return Ok(user);
+        }
+        catch (NumberedException nEx)
+        {
+            _logger.LogWithLevel(LogLevel.Error, nEx, nEx.ErrorNumber, nEx.Message,
+                nameof(ShoppingListController), nameof(GetUserById));
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Due to an internal error, your request could not be processed.");
+        }
+        catch (Exception e)
+        {
+            var numberedException = new NumberedException(e);
+            _logger.LogWithLevel(LogLevel.Error, e, numberedException.ErrorNumber, numberedException.Message,
+                nameof(ShoppingListController), nameof(GetUserById));
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Due to an internal error, your request could not be processed.");
+        }
+    }
+
+    [HttpGet]
+    [AdminEndpoint]
+    [ProducesResponseType<List<ListUserMinimal>>(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType<string>(StatusCodes.Status500InternalServerError)]
+    [Route("User/all")]
+    public async Task<ActionResult> GetAllUsers()
+    {
+        try
+        {
+            var users = await _databaseService.SqlConnectionHandler<List<ListUserMinimal>>(
+                async connection => await _databaseService.GetAllUsers(connection));
+
+            if (users.Count == 0)
+            {
+                return NoContent();
+            }
+
+            return Ok(users);
+        }
+        catch (NumberedException nEx)
+        {
+            _logger.LogWithLevel(LogLevel.Error, nEx, nEx.ErrorNumber, nEx.Message,
+                nameof(ShoppingListController), nameof(GetAllUsers));
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Due to an internal error, your request could not be processed.");
+        }
+        catch (Exception e)
+        {
+            var numberedException = new NumberedException(e);
+            _logger.LogWithLevel(LogLevel.Error, e, numberedException.ErrorNumber, numberedException.Message,
+                nameof(ShoppingListController), nameof(GetAllUsers));
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Due to an internal error, your request could not be processed.");
+        }
+    }
+    
+    [HttpPost]
+    [AdminEndpoint]
+    [ProducesResponseType<string>(StatusCodes.Status409Conflict)]
+    [ProducesResponseType<Guid>(StatusCodes.Status201Created)]
+    [ProducesResponseType<string>(StatusCodes.Status500InternalServerError)]
+    [Route("UserRole")]
+    public async Task<ActionResult> AddUserRole([FromBody] UserRolePost userRolePost)
+    {
+        try
+        {
+            var alreadyExists = await _databaseService.SqlConnectionHandler<int, bool>(
+                async (enumIndex, sqlConnection) =>
+                    await _databaseService.CheckUserRoleExistence(enumIndex, sqlConnection),
+                (int)userRolePost.UserRoleEnum
+            );
+
+            if (alreadyExists)
+            {
+                return Conflict("A user role withe same Enum index already exists in the database.");
+            }
+
+            var (success, addedUserRoleId) = await _databaseService.SqlConnectionHandler<UserRolePost, (bool, Guid?)>(
+                (input, connection) => _databaseService.AddUserRole(connection, input), userRolePost);
+
+            if (success is false)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                    "Du to an internal error, your request could not be processed.");
+            }
+
+            return CreatedAtAction(nameof(AddUserRole), addedUserRoleId);
+        }
+        catch (NumberedException nEx)
+        {
+            _logger.LogWithLevel(LogLevel.Error, nEx, nEx.ErrorNumber, nEx.Message,
+                nameof(ShoppingListController), nameof(AddUserRole));
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Du to an internal error, your request could not be processed.");
+        }
+        catch (Exception e)
+        {
+            var numberedException = new NumberedException(e);
+            _logger.LogWithLevel(LogLevel.Error, e, numberedException.ErrorNumber, numberedException.Message,
+                nameof(ShoppingListController), nameof(AddUserRole));
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Du to an internal error, your request could not be processed.");
+        }
+    }
+
+    [HttpPost]
+    [PublicEndpoint]
+    [ProducesResponseType<string>(StatusCodes.Status409Conflict)]
+    [ProducesResponseType<Guid>(StatusCodes.Status201Created)]
+    [ProducesResponseType<string>(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType<string>(StatusCodes.Status500InternalServerError)]
+    [Route("User")]
+    public async Task<ActionResult> AddNewUser([FromBody] ListUserPost userPost)
+    {
+        try
+        {
+            var emailAlreadyExists = await _databaseService.SqlConnectionHandler<string, bool>(
+                async (emailAddress, sqlConnection) =>
+                    await _databaseService.CheckUserExistence(emailAddress, sqlConnection), userPost.EmailAddress);
+
+            if (emailAlreadyExists)
+            {
+                return Conflict(
+                    "A user with the same email address already exists in the database. Use a different address to register yourself!");
+            }
+
+            var userPostExtended = new ListUserPostExtended(userPost);
+
+            var (success, addedUserId) =
+                await _databaseService.SqlConnectionHandler<ListUserPostExtended, (bool, Guid?)>(
+                    (input, connection) => _databaseService.AddUser(input, connection), userPostExtended);
+
+            if (success is false || addedUserId is null)
+            {
+                return Problem("Due to an internal error, you request could not be processed.");
+            }
+
+            return Ok(addedUserId);
+        }
+        catch (FormatException fEx)
+        {
+            _logger.LogWithLevel(LogLevel.Error, fEx, "0", fEx.Message,
+                nameof(ShoppingListController), nameof(AddUserRole));
+            return BadRequest("Your input email address and password were delivered in wrong formatting!");
+        }
+        catch (NumberedException nEx)
+        {
+            _logger.LogWithLevel(LogLevel.Error, nEx, nEx.ErrorNumber, nEx.Message,
+                nameof(ShoppingListController), nameof(AddUserRole));
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Due to an internal error, your request could not be processed.");
+        }
+        catch (Exception e)
+        {
+            var numberedException = new NumberedException(e);
+            _logger.LogWithLevel(LogLevel.Error, e, numberedException.ErrorNumber, numberedException.Message,
+                nameof(ShoppingListController), nameof(AddNewUser));
+            return StatusCode(StatusCodes.Status500InternalServerError,
+                "Due to an internal error, your request could not be processed.");
+        }
+    }
+
     [HttpPost]
     [Route("User/Login")]
     [PublicEndpoint]
     [ProducesResponseType<ListUser>(StatusCodes.Status200OK)]
     [ProducesResponseType<string>(StatusCodes.Status401Unauthorized)]
+    [ProducesResponseType<string>(StatusCodes.Status500InternalServerError)]
     public async Task<ActionResult> UserLogin([FromHeader] string emailAddress, [FromHeader] string password)
     {
         try
@@ -217,74 +370,9 @@ public class ShoppingListController : ControllerBase
                 "Due to an internal error, your request could not be processed.");
         }
     }
+    
+    /*
 
-    /*[HttpPost]
-    [Route("base64test")]
-    public ActionResult Base64Test([FromBody] string base64String)
-    {
-        byte[] decodedBytes = Convert.FromBase64String(base64String);
-
-        // Convert byte array to a readable string (assuming UTF-8 encoding)
-        string decodedString = Encoding.UTF8.GetString(decodedBytes);
-
-        return Ok(decodedString);
-    }
-
-    [HttpPost]
-    [Route("HashTest")]
-    public ActionResult HashTest([FromBody] string password)
-    {
-        string testHashedPass = BCrypt.Net.BCrypt.EnhancedHashPassword("testPass1", 13);
-
-        bool verified = BCrypt.Net.BCrypt.EnhancedVerify(password, testHashedPass);
-
-        if (verified)
-        {
-            return Ok();
-        }
-        else
-        {
-            return Unauthorized();
-        }
-
-    }*/
-
-    /*a
-
-    [HttpGet]
-    [Route("User/{userId:guid}")]
-    public async Task<ActionResult> GetUserById(Guid userId)
-    {
-        try
-        {
-            var user = await _databaseService.SqlConnectionHandler<Guid, ListUser?>(
-                (input, connection) => _databaseService.GetUserById(input, connection),
-                userId
-            );
-
-            if (user == null)
-            {
-                return NotFound($"User with ID {userId} not found");
-            }
-
-            return Ok(user);
-        }
-        catch (NumberedException nEx)
-        {
-            _logger.LogWithLevel(LogLevel.Error, nEx, nEx.ErrorNumber, nEx.Message,
-                nameof(ShoppingListController), nameof(GetUserById));
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                "Due to an internal error, your request could not be processed.");
-        }
-        catch (Exception e)
-        {
-            var numberedException = new NumberedException(e);
-            _logger.LogWithLevel(LogLevel.Error, e, numberedException.ErrorNumber, numberedException.Message,
-                nameof(ShoppingListController), nameof(GetUserById));
-            return StatusCode(StatusCodes.Status500InternalServerError,
-                "Due to an internal error, your request could not be processed.");
-        }
-    }
 
     [HttpGet]
     [Route("ShoppingList/{listId:guid}")]
